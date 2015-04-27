0a0a4fa10f2e646ed52bb1b1affa7b92a2211d38
Tom Neyland <tcneyland+github@gmail.com>
2015-04-27 17:28:11 -0400

    fix(app): Import angular before using it

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,8 @@
+// load npm/browserify modules
+import angular from 'angular';
+
+//load our modules
 import EventBus from './services/EventBus';
+
+
 angular.module('test', []).service('EventBus', EventBus);
